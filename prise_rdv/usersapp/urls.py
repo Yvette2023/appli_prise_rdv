17836ce7b4aa55345fb1c6fdc4d561c61d4814b1
Ingabire Yvette <yvette.ingabire@live.fr>
2023-02-07 16:37:10 +0100

    authentification

--- prise_rdv/usersapp/urls.py
+++ prise_rdv/usersapp/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns =[
 	path('accounts/', include('django.contrib.auth.urls')),
+ 	path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name = 'login'),
 ]
